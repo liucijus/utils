9834090c6f75774d9448f69951ef570245835d8f
Vaidas Pilkauskas <vaidas.pilkauskas@gmail.com>
2016-04-25 09:17:53 +0300

    Update dispaly setup and refactor

--- work_display_setup.sh
+++ work_display_setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-xrandr --output DP2 --mode 1920x1200 --output VGA1 --mode 1920x1200 --right-of DP2 --output eDP1 --mode 1600x900 --right-of VGA1
+HEAD_1=("DP2-2" "1920x1200")
+HEAD_2=("VGA1" "1920x1200")
+HEAD_3=("eDP1" "1600x900")
+
+xrandr \
+  --output ${HEAD_1[0]} --mode ${HEAD_1[1]} \
+  --output ${HEAD_2[0]} --mode ${HEAD_2[1]} --right-of ${HEAD_1[0]} \
+  --output ${HEAD_3[0]} --mode ${HEAD_3[1]} --right-of ${HEAD_2[0]}
  
