e009db490bd9efcf299812305d7dfb3806b4b971
Vaidas Pilkauskas <vaidas.pilkauskas@gmail.com>
2016-04-26 09:21:38 +0300

    Refactor multihead setup script

--- work_display_setup.sh
+++ work_display_setup.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-HEAD_1=("DP2-2" "1920x1200")
-HEAD_2=("VGA1" "1920x1200")
-HEAD_3=("eDP1" "1600x900")
+HEAD_1="DP2-2 1920x1200"
+HEAD_2="VGA1 1920x1200"
+HEAD_3="eDP1 1600x900"
 
-xrandr \
-  --output ${HEAD_1[0]} --mode ${HEAD_1[1]} \
-  --output ${HEAD_2[0]} --mode ${HEAD_2[1]} --right-of ${HEAD_1[0]} \
-  --output ${HEAD_3[0]} --mode ${HEAD_3[1]} --right-of ${HEAD_2[0]}
- 
+HEADS=( $HEAD_1 $HEAD_2 $HEAD_3 )
+
+XRANDR_CMD="xrandr"
+
+MAX_IDX=$(( ${#HEADS[@]} - 2 )) 
+
+for i in `seq 0 2 $MAX_IDS` 
+do
+  NAME_IDX=$(( i * 2 ))
+  MODE_IDX=$(( $NAME_IDX + 1 ))
+  XRANDR_CMD+=" --output ${HEADS[$NAME_IDX]} --mode ${HEADS[$MODE_IDX]}" 
+  if (( $i > 0 ))
+  then
+    PREV_NAME_IDX=$(( $NAME_IDX - 2 ))
+    XRANDR_CMD+=" --right-of ${HEADS[$PREV_NAME_IDX]}"
+  fi  
+done
+
+echo $XRANDR_CMD
+`$XRANDR_CMD`
+
